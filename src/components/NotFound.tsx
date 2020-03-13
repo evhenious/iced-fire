37a5318849c0f664f34a560d999080f955346b83
Yevhen Hraivoronskyi <yevhen.hraivoronskyi@altexsoft.com>
2020-03-13 16:32:38 +0200

    slightly updated NotFound page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+const style = {
+  padding: '10px',
+  textAlign: 'center' as const,
+  color: 'coral',
+  width: '30vw',
+  transform: 'translate(-50%, -50%)',
+  position: 'absolute' as const,
+  left: '50%',
+  top: '30%'
+};
+
 const NotFound: React.FC = () => {
   return (
-    <div>
-      <div>Invalid address. Probably you need this link:</div>
+    <div style={style}>
+      <div>Invalid address. Try this link instead:</div>
       <Link to={'/'}>Home page - Characters list</Link>
     </div>
   );
